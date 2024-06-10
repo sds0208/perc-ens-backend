8ef3f4894018d2ece6354dcca6cc2937428cd802
sds0208 <sarahsantacruz86@gmail.com>
2024-06-10 11:08:15 -0500

    removed commented code

--- src/ensemble/db.js
+++ src/ensemble/db.js
@@ -1,15 +1,6 @@
 require('dotenv').config();
 const Pool = require('pg').Pool;
 
-// const pool = new Pool({
-//     user: process.env.DBUSER,
-//     host: process.env.HOST,
-//     database: process.env.DBNAME,
-//     port: process.env.DB_PORT,
-//     password: process.env.PASSWORD,
-//     ssl: process.env.SSL
-// });
-
 const pool = new Pool({
     connectionString: process.env.DB_CONNECTION_STRING
 })
