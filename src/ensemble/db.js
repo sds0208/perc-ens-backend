8acef6b4647e74dafeeaf0eadd062d7c9bf70a48
sds0208 <sarahsantacruz86@gmail.com>
2024-06-10 11:04:06 -0500

    updated db config

--- src/ensemble/db.js
+++ src/ensemble/db.js
@@ -1,13 +1,17 @@
 require('dotenv').config();
 const Pool = require('pg').Pool;
 
+// const pool = new Pool({
+//     user: process.env.DBUSER,
+//     host: process.env.HOST,
+//     database: process.env.DBNAME,
+//     port: process.env.DB_PORT,
+//     password: process.env.PASSWORD,
+//     ssl: process.env.SSL
+// });
+
 const pool = new Pool({
-    user: process.env.DBUSER,
-    host: process.env.HOST,
-    database: process.env.DBNAME,
-    port: process.env.PORT,
-    password: process.env.PASSWORD,
-    ssl: process.env.SSL
-});
+    connectionString: process.env.DB_CONNECTION_STRING
+})
 
 module.exports = pool;
