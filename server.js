549f9d6370787c2924fe639a1ff9603fda6c039c
sds0208 <sarahsantacruz86@gmail.com>
2024-06-10 08:15:06 -0500

    small cors update

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 // app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://percussionensembledatabase.netlify.app");
   res.setHeader("Access-Control-Allow-Methods", "GET");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
