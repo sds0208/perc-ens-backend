ad98f62f0d5650f09454885b31c1f642c0f06a94
sds0208 <sarahsantacruz86@gmail.com>
2024-06-10 07:54:21 -0500

    updated cors for production

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-    origin: '*',
+    origin: 'https://percussionensembledatabase.netlify.app/',
     optionsSuccessStatus: 200,
   };
  
